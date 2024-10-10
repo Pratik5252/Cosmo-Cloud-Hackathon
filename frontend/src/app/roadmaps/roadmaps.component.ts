c0ea18aec864b7574a0ba97705cd1e4f90f01975
Pratik Yesane <pratiky2000@gmail.com>
2024-10-10 21:25:33 +0530

    Update Roadmap Component

--- frontend/src/app/roadmaps/roadmaps.component.ts
+++ frontend/src/app/roadmaps/roadmaps.component.ts
@@ -1,37 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { GeminiService } from '../services/gemini/gemini.service';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roadmaps',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './roadmaps.component.html',
-  styleUrl: './roadmaps.component.css',
+  styleUrls: ['./roadmaps.component.css'],
 })
-export class RoadmapsComponent implements OnInit {
-  inputField: string = '';
-  result: string = '';
-  loading: boolean = false;
-
-  constructor(private geminiService: GeminiService) {}
-
-  ngOnInit(): void {}
-
-  async generateRoadmap() {
-    this.loading = true;
-
-    try {
-      const resultText = await this.geminiService.generateRoadmap(
-        this.inputField
-      );
-      this.result = resultText;
-      console.log(this.result);
-    } catch (error) {
-      console.error('Error:', error);
-    } finally {
-      this.loading = false;
-    }
-  }
-}
+export class RoadmapsComponent {}
