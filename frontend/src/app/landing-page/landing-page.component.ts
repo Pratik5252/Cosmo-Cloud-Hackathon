28011b090eafb927d9189314bbbb545f1497e40d
Pratik Yesane <pratiky2000@gmail.com>
2024-10-15 23:34:23 +0530

    Small Changes

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-// import { AuthService } from '../services/auth/auth.service';
+import { AuthService } from '../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { NavigationService } from '../services/navigation/navigation.service';
-// import { AuthOperationsService } from '../services/auth/auth-operation.service';
+import { AuthOperationsService } from '../services/auth/auth-operation.service';
 import {
   NgIconComponent,
   provideIcons,
@@ -28,16 +28,17 @@ export class LandingPageComponent {
   isLoggedIn: boolean = false;
 
   constructor(
-    // public authService: AuthService,
-    private navigationService: NavigationService // private authOperations: AuthOperationsService
+    public authService: AuthService,
+    private navigationService: NavigationService,
+    private authOperations: AuthOperationsService
   ) {
-    // this.authService.user$.subscribe((user) => {
-    //   this.isLoggedIn = !!user;
-    // });
+    this.authService.user$.subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
   }
 
   async handleLogin() {
-    // this.authOperations.login();
+    this.authOperations.login();
   }
 
   navigateToRoadmaps() {
--- frontend/src/app/services/auth/auth-operation.service.ts
+++ frontend/src/app/services/auth/auth-operation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-// import { AuthService } from '../auth/auth.service';
+import { AuthService } from '../auth/auth.service';
 import { NavigationService } from '../navigation/navigation.service';
 
 @Injectable({
@@ -7,28 +7,28 @@ import { NavigationService } from '../navigation/navigation.service';
 })
 export class AuthOperationsService {
   constructor(
-    // private authService: AuthService,
+    private authService: AuthService,
     private navigationService: NavigationService
   ) {}
 
   async login(): Promise<void> {
-    // const user = await this.authService.signInWithGoogle();
-    // if (user) {
-    //   console.log('User logged in:', user);
-    //   this.navigationService.navigateToLandingPage();
-    // } else {
-    //   console.error('Login failed');
-    // }
+    const user = await this.authService.signInWithGoogle();
+    if (user) {
+      console.log('User logged in:', user);
+      this.navigationService.navigateToLandingPage();
+    } else {
+      console.error('Login failed');
+    }
   }
 
   async logout(): Promise<void> {
-    // await this.authService.signOut();
+    await this.authService.signOut();
     console.log('User logged out');
     this.navigationService.navigateToLandingPage();
   }
 
-  // async loginWithDifferentAccount(): Promise<void> {
-  //   await this.authService.signOut();
-  //   await this.login();
-  // }
+  async loginWithDifferentAccount(): Promise<void> {
+    await this.authService.signOut();
+    await this.login();
+  }
 }
