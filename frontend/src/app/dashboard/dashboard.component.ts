d9bf692bae665592d4aad01a8ef6db701e3c4f96
Pratik Yesane <pratiky2000@gmail.com>
2024-10-20 22:03:25 +0530

    Added Dashboard UI

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { NavigationService } from '../services/navigation/navigation.service';
+import {
+  NgIconComponent,
+  provideIcons,
+  provideNgIconsConfig,
+} from '@ng-icons/core';
+import {
+  matChatBubble,
+  matArrowOutward,
+} from '@ng-icons/material-icons/baseline';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  providers: [
+    provideIcons({ matChatBubble, matArrowOutward }),
+    provideNgIconsConfig({
+      size: '20px',
+    }),
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  constructor(private navigationService: NavigationService) {}
 
+  navigateToRoadmaps() {
+    this.navigationService.navigateToRoadmaps();
+  }
 }
