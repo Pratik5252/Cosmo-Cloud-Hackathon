3e8699909722b91e456f92aa31e17b744b8e73b7
Pratik Yesane <pratiky2000@gmail.com>
2024-10-21 20:10:57 +0530

    Langing Page UI update

--- frontend/src/app/study/study.component.ts
+++ frontend/src/app/study/study.component.ts
@@ -1,23 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import   
- { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'; 1    
-
+// import
+//  { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fas } from '@fortawesome/free-solid-svg-icons'; 1
 
 @Component({
   selector: 'app-study',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './study.component.html',
-  styleUrl: './study.component.css'
+  styleUrl: './study.component.css',
 })
 export class StudyComponent {
-  studyBuddyName: string | null = "John Doe"; // Replace with logic to get study buddy name
-  studyBuddyJobTitle: string | null = "Senior Developer";
-  studyBuddySkills: string | null = "Java, Python, Angular";
+  studyBuddyName: string | null = 'John Doe'; // Replace with logic to get study buddy name
+  studyBuddyJobTitle: string | null = 'Senior Developer';
+  studyBuddySkills: string | null = 'Java, Python, Angular';
   constructor(private router: Router) {}
 
   chatWithStudyBuddy() {
